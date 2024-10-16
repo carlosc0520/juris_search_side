b13f8ca278785d9fdd8dc06144581d2617e0e4b9
carlosc0520 <ccarbajalmt0520@gmail.com>
2024-10-16 17:52:52 -0500

    aa

---
+++ dist/js/113.7df054d3.js
@@ -0,0 +1,21 @@
+/*!
+
+=========================================================
+* Vue Notus - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vue-notus
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/vue-notus/blob/main/LICENSE.md)
+
+* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+"use strict";(self["webpackChunkvue_notus"]=self["webpackChunkvue_notus"]||[]).push([[113],{2113:function(e,t,a){a.r(t),a.d(t,{default:function(){return g}});var n=a(8231),o={class:"bg-white"},l={class:"relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75"};function s(e,t,a,s,r,c){var i=(0,n.resolveComponent)("navbar"),m=(0,n.resolveComponent)("footer-component");return(0,n.openBlock)(),(0,n.createElementBlock)("div",null,[(0,n.createVNode)(i),(0,n.createElementVNode)("main",o,[(0,n.createElementVNode)("div",l,[(0,n.createElementVNode)("div",{class:"absolute top-0 w-full h-full bg-center bg-cover",style:(0,n.normalizeStyle)("background-image: url(".concat(r.backgroundImage,"); background-size: cover; background-position: center;"))},t[0]||(t[0]=[(0,n.createElementVNode)("span",{id:"blackOverlay",class:"w-full h-full absolute opacity-75 bg-black"},null,-1)]),4),t[1]||(t[1]=(0,n.createStaticVNode)('<div class="container relative mx-auto p-5"><div class="items-center flex flex-wrap"><div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center"><div class=""><h1 class="text-white font-semibold text-5xl"> últimas noticias </h1><p class="mt-4 text-lg text-blueGray-200"> Aqui encontrarás las últimas noticias de Juris Search. </p></div></div></div></div><div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" style="transform:translateZ(0);"><svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0" fill="white"><polygon class="text-white white bg-white fill-current" points="2560 0 2560 100 0 100"></polygon></svg></div>',2))]),t[2]||(t[2]=(0,n.createElementVNode)("div",{class:"container mx-auto px-4 mt-5 my-0"},[(0,n.createElementVNode)("h2",{class:"text-center text-xl font-bold mb-2"},"Síguenos en Instagram"),(0,n.createElementVNode)("p",{class:"text-center"},"Descubre nuestras últimas publicaciones y actualizaciones."),(0,n.createElementVNode)("iframe",{src:"https://www.instagram.com/juris.search/embed",class:"frame-instagram m-0",frameborder:"0",scrolling:"no",allowfullscreen:""})],-1))]),(0,n.createVNode)(m)])}var r=a(3435),c=a(2891),i=a(7020),m=a(3307),u=a(7050),d=a(1051),v=a(8929),p={data:function(){return{team1:i.A,team2:m.A,team3:u.A,team4:v.A,backgroundImage:d.A}},components:{Navbar:c.A,FooterComponent:r.A}},b=a(6262);const f=(0,b.A)(p,[["render",s]]);var g=f}}]);
+//# sourceMappingURL=113.7df054d3.js.map
